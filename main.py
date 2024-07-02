c75cda5e2d1b5a5150c74a128255432f62e55177
Jake Frischmann <53714729+JJFrisch@users.noreply.github.com>
2024-07-02 16:20:38 +0000

    added a first atempt at getting all possible moves and placing a piece

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ from board import Board
 from orient import generatePiecesDict, rotate
 
 #read from file to get the pieces arrays
+orientation = [[0,0,[0,0], [0,0]], [[0,0], [1,1]], [[0,0], [99,1]], [[0,0], [1,1]], [[0,0], [1,1]] ]
+NE,SE,SW,NW = orientation[1:]
+print(SE)
 
-board = Board(14, pieces)
 
-# new_pieces = generatePiecesDict(pieces)
-board.print()
+# board = Board(14, pieces)
 
-# board.place(5, 5, 12, 0, 1)
-print(board.legalMoves())
+# # new_pieces = generatePiecesDict(pieces)
+# board.print()
+
+# # board.place(5, 5, 12, 0, 1)
+# print(board.legalMoves())
 
 # print(new_pieces[12][0])
 # print(new_pieces[12][4])  # to check the generatePiecesDict function
@@ -19,14 +23,13 @@ print(board.legalMoves())
 while board.running:
   if board.state == 'p1_turn':
     #player 1's turn
-    
+
     # board.print()
     break
     # what is it that happens on each turn?
-    # 
+    #
     # identify places to put pieces
 
-  
   if board.state == 'p2_turn':
     #player 2's turn
     board.print()
