c96eba942851827f14ad21772f888588c123addc
etashj <etashjhanji@gmail.com>
2024-07-11 17:09:54 -0400

    research plus setup

---
+++ blokusEnv.py
@@ -0,0 +1,4 @@
+## DEFINING A BLOCKUS ENVIRONMENT IN OPENAI GYM FOR RL
+
+# https://medium.com/@paulswenson2/an-introduction-to-building-custom-reinforcement-learning-environment-using-openai-gym-d8a5e7cf07ea
+
