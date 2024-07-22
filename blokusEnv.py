f5524feda9e53b33e1a45337075e66cfba60f2c9
etashj <etashjhanji@gmail.com>
2024-07-22 15:15:58 -0400

    Revamped entirely to fix all logic, random moves + neg reward for illegal moves

--- blokusEnv.py
+++ blokusEnv.py
@@ -17,78 +17,98 @@ class BlokusEnv(ParallelEnv):
         self.board = Board(14)                           # Blokus board
         # self.state = self.board.asFlatNumpyArr()         # Flattened 14 by 14 (196) matrix of the board
         self.possible_agents = [0,1]
+        self.wins= [0,0]
+        self.done = False
 
     def reset(self, seed=None, options=None):
         self.agents = copy(self.possible_agents)
+        del self.board
         self.board = Board(14)                           # Blokus board
+        self.done = False
         # self.state = self.board.asFlatNumpyArr()         # Flattened 14 by 14 (196) matrix of the board
 
+        print(self.action_space(0))
         observations = {
-            # 0: {
-            #     'board': self.state,
-            #     'pieces': self.board.inv,
-            # }, 
-            # 1: {
-            #     'board': self.state,
-            #     'pieces': self.board.inv,
-            # }, 
             0: {
                 'board': np.array(self.board.board),
-                'pieces': np.ones([2,20]), 
+                'pieces': np.ones([2,20]),
+                # 'action_mask': self._get_action_mask(0),  # Initialize action mask
             }, 
             1: {
                 'board': np.array(self.board.board),
                 'pieces': np.ones([2,20]), 
+                # 'action_mask': self._get_action_mask(0),  # Initialize action mask
             }, 
         }
         infos = {a: {} for a in self.agents}
         return observations, infos
 
     def step(self, actions):
+        print(actions)
+
+        rewards = {agent: 0 for agent in self.agents}
+        terminations = {0: False, 1: False}
         #Get actions
-        action1 = actions[0]
-        action2 = actions[1]
-
-        # Do the actions
-        action_mask = [0,0,0,0,0]; 
-
-
-        rewards = {0: 0, 1: 0}
-
         # Check if the move is valid and place it + reward
         # TODO: Action mask invalid moves
-        valid1Found = False
-        for action in self.board.calculateLegalMoves(): 
-            if action1[0] == action[0] and action1[1] == action[1] and action1[2] == action[2] and action1[3] == action[3] and action1[4] == action[5]: 
-                self.board.place_piece(action)
-                rewards[0] = piecesLenKey[action1[2]-1]
-                valid1Found = True
-                break
-        if not valid1Found:
-            rewards[0] = -50
+        if 0 in self.agents: 
+            action1 = actions[0]
+            # action1 = self._index_to_action(actions[0], self.action_space(0))
+            valid1Found = False
+            for action in self.board.calculateLegalMoves(): 
+                if action1[0] == action[0] and action1[1] == action[1] and action1[2] == action[2] and action1[3] == action[3]: 
+                    self.board.place_piece(action)
+                    rewards[0] = piecesLenKey[action1[2]-1]
+                    valid1Found = True
+                    self.board.switchPlayer()
+                    break
+            if not valid1Found:
+                self.board.randomTurn(None, None)
+                rewards[0] = -50
+            if self.board.calculateLegalMoves()==[]:
+                terminations[0] = True
 
-        self.board.switchPlayer()
-
-        valid2Found = False
-        for action in self.board.calculateLegalMoves(): 
-            if action2[0] == action[0] and action2[1] == action[1] and action2[2] == action[2] and action2[3] == action[3] and action2[4] == action[5]: 
-                self.board.place_piece(action)
-                rewards[1] = piecesLenKey[action2[2]-1]
-                valid2Found = True
-                break
-        if not valid2Found:
-            rewards[1] = -50
-
-        self.board.switchPlayer()
-        
-        # self.state = self.board.asFlatNumpyArr()
+        if 1 in self.agents: 
+            action2 = actions[1]
+            valid2Found = False
+            for action in self.board.calculateLegalMoves(): 
+                if action2[0] == action[0] and action2[1] == action[1] and action2[2] == action[2] and action2[3] == action[3]: 
+                    self.board.place_piece(action)
+                    rewards[1] = piecesLenKey[action2[2]-1]
+                    valid2Found = True
+                    self.board.switchPlayer()
+                    break
+            if not valid2Found:
+                self.board.randomTurn(None, None)
+                rewards[1] = -50
+            if self.board.calculateLegalMoves()==[]:
+                terminations[1] = True
+            # action2 = self._index_to_action(actions[1], self.action_space(1))
 
         # Check if game is over
-        terminations = {0: False, 1: False}
-        if self.board.finished[0]: 
+        if 0 not in self.agents: 
             terminations[0] = True
-        if self.board.finished[1]: 
+        elif 0 in self.agents and self.board.calculateLegalMoves() == []: 
+            terminations[0] = True
+        self.board.switchPlayer()
+        if 1 not in self.agents: 
+            terminations[0] = True
+        elif 1 in self.agents and self.board.calculateLegalMoves() == []: 
             terminations[1] = True
+        self.board.switchPlayer()
+        
+        
+        if terminations[0] and terminations[1]:
+            if self.board.score[0] > self.board.score[1]:
+                rewards[0] += 100
+                rewards[1] -= 100
+                self.wins[0] += 1
+            elif self.board.score[0] < self.board.score[1]:
+                rewards[0] -= 100
+                rewards[1] += 100
+                self.wins[1] += 1
+            self.done = True
+            # self.reset()
 
         truncations = {a: False for a in self.agents}
 
@@ -99,28 +119,28 @@ class BlokusEnv(ParallelEnv):
             if piece in self.board.inv[1]: 
                 piecesMB[1][piece-1] = 1
 
-
+        # action_masks = {a: self._get_action_mask(a) for a in self.agents}
         observations = {
-            # 0: {
-            #     'board': self.state,
-            #     'pieces': piecesMB,
-            # }, 
-            # 1: {
-            #     'board': self.state,
-            #     'pieces': piecesMB,
-            # },
             0: {
                 'board': np.array(self.board.board),
-                'pieces': np.ones([2,20]), 
+                'pieces': np.array(piecesMB), 
+                # 'action_mask': action_masks[0],  # Include action mask
             }, 
             1: {
                 'board': np.array(self.board.board),
-                'pieces': np.ones([2,20]), 
+                'pieces': np.array(piecesMB), 
+                # 'action_mask': action_masks[1],  # Include action mask
             },  
         }
 
         infos = {a: {} for a in self.agents}
 
+        self.render()
+
+        if 0 in self.agents and terminations[0]: 
+            self.agents.remove(0)
+        if 1 in self.agents and terminations[1]: 
+            self.agents.remove(1)
         return observations, rewards, terminations, truncations, infos
 
     def render(self):
@@ -131,7 +151,6 @@ class BlokusEnv(ParallelEnv):
         return spaces.Dict({
             "board": spaces.Box(low=0, high=2, shape=(14, 14), dtype=np.int8),  # self.state - flattened board arr - {0:"unused", 1:"p1 block", 2:"p2 block"}
             "pieces": spaces.MultiBinary([2, 20]),                              # pieces left for each player 
-            # "action_mask": spaces.Box(low=0, high=1, shape=(14, 14, 21, 8, 4), dtype=np.int8)  # action mask
         })
     
     @functools.lru_cache(maxsize=None)
@@ -142,11 +161,14 @@ class BlokusEnv(ParallelEnv):
             spaces.Discrete(21, start=1),   # Piece Number [1,21]
             spaces.Discrete(8),             # 4 rotations * 2 flips
                                             # poss squares index is only really for computation, can vary too much for model prediction;
-            spaces.Discrete(4)              # dir [0,3]
+                                            # dir [0,3] not neccesary
         ))
     
+    def getWins(self): 
+        return self.wins
+
 
 if __name__ == "__main__":
     from pettingzoo.test import parallel_api_test
     env = BlokusEnv()
-    parallel_api_test(env, num_cycles=50)
+    parallel_api_test(env, num_cycles=500000)
